cf255b115b0570165f742ed73bb6eed4e3cbba25
Doğukan Atalay <dogukanatalay46@gmail.com>
2024-01-10 22:27:37 +0300

    added read and delete

--- src/components/FileForm.vue
+++ src/components/FileForm.vue
@@ -1,29 +1,76 @@
 <template>
-    <div id="form">
-        <div style="display: flex; flex-direction: column;">
-            <label for="key">Key:</label>
-            <textarea v-model="secretKey" name="" id="" cols="30" rows="10"></textarea>
+    <div>
+
+        <div id="form">
+            <div style="display: flex; flex-direction: column;">
+                <label for="key">Key:</label>
+                <textarea v-model="secretKey" name="" id="" cols="30" rows="10"></textarea>
+            </div>
+
+            <div id="algo-pick">
+                <label for="algo">Algorithm: </label>
+                <select name="" id="" v-model="selectedAlgo">
+                    <option v-for="algo in hashAlgoritms" :key="algo">{{ algo }}</option>
+                </select>
+            </div>
+
+            <div id="file-input">
+                <input style="width: 90px;" type="file" @change="handleFileUpload">
+
+                <button @click="createHashedFile()" class="btn">
+                    Upload
+                </button>
+            </div>
+
         </div>
 
-        <div id="algo-pick">
-            <label for="algo">Algorithm: </label>
-            <select name="" id="" v-model="selectedAlgo">
-                <option v-for="algo in hashAlgoritms" :key="algo">{{ algo }}</option>
-            </select>
+        <div id="view-files">
+            <div>
+                <button @click="getAESfile()" class="btn" style="width: 80px;">
+                    Decrypt and get AES file
+                </button>
+            </div>
+
+            <div>
+                <button @click="deleteFile()" class="btn" style="width: 80px;">
+                    Delete Selected file
+                </button>
+            </div>
+
+            <div v-if="imgSrc">
+                <img :src="imgSrc" style="width: 200px;" alt="">
+            </div>
+
+            <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+
+            <img :src="imgSrc" alt="">
         </div>
 
-        <div id="file-input">
-            <input style="width: 90px;" type="file" @change="handleFileUpload">
-
-            <button @click="createHashedFile()" class="btn">
-                Upload
-            </button>
+        <div v-if="exception" style="color: red">
+            {{ exception }}
         </div>
 
+        <ul id="all-files">
+            <h2>
+                All files in /upload:
+            </h2>
+            <li v-for="file in allFiles" :key="file">
+                <span> {{ file }} </span>
+                <button 
+                    @click="selectedFileName = file"
+                    class="btn" 
+                    style="margin-left: 10px;"
+                >
+                    Select
+                </button>
+
+            </li>
+        </ul>
+
+        <div>
+            Selected file name: {{ selectedFileName }}
+        </div>
     </div>
-
-    <div v-if="exception" style="color: red">{{ exception }}</div>
-
 </template>
   
 <script>
@@ -36,14 +83,57 @@ export default {
             hashAlgoritms: ["none", "DES", "AES", "Blowfish"],
             selectedAlgo: "",
             selectedFile: null,
-            secretKey: "",
-            exception: null
+            secretKey: "aabbccddeeff00112233445566778899aabbccddeeff00112233445566778899",
+            exception: null,
+            imgSrc: null,
+            allFiles: [],
+            selectedFileName: ""
         }
+    },
+    created() {
+        this.getAllFiles();
     },
     methods: {
         handleFileUpload(event) {
             this.selectedFile = event.target.files[0];
         },
+
+        async getAllFiles() {
+            let response = await DataService.getAllFiles();
+            this.allFiles = response.data;
+            console.log('files: ', response);
+        },
+
+        async deleteFile() {
+            let response = await DataService.deleteFile(this.selectedFileName);
+            console.log('res: ', response);
+
+            await this.getAESfile();
+        },
+
+        bufferToBase64(buf) {
+            var binary = '';
+            var bytes = new Uint8Array(buf);
+            var len = bytes.byteLength;
+            for (var i = 0; i < len; i++) {
+                binary += String.fromCharCode(bytes[i]);
+            }
+            return window.btoa(binary);
+        },
+
+        async getAESfile() {
+            try {
+                let response = await DataService.getAESfile(this.selectedFileName);
+                let imgData = response.data.decryptedFile.data;
+                let base64String = this.bufferToBase64(imgData);
+                this.imgSrc = 'data:image/png;base64,' + base64String;
+
+                console.log(this.imgSrc);
+            } catch (ex) {
+                this.exception = ex;
+            }
+        },
+
         async createHashedFile() {
             const formData = new FormData();
             formData.append("file", this.selectedFile);
@@ -99,5 +189,21 @@ export default {
 
 #algo-pick {
     margin-left: 10px;
+}
+
+#view-files {
+    box-shadow: 3px 3px 3px 3px lightgray;
+    border: none;
+    border-radius: 6px;
+    margin-top: 20px;
+    width: 600px;
+    padding: 10px;
+    
+}
+
+#all-files {
+    box-shadow: 3px 3px 3px 3px lightgray;
+    padding: 30px;
+    padding-top: 10px;
 }
 </style>
--- src/services/DataService.js
+++ src/services/DataService.js
@@ -8,6 +8,18 @@ class DataService {
             }
         });
     }
+
+    getAESfile(selectedFile) {
+        return http.get(`/file/aes/${selectedFile}`);
+    }
+
+    getAllFiles() {
+        return http.get("/files/all");
+    }
+
+    deleteFile(selectedFile) {
+        return http.delete(`/files/delete/${selectedFile}`)
+    }
 }
 
 export default new DataService();
